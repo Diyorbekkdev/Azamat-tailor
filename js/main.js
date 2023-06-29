48b7c0c908c8d820c2dca28591f23213aeb652e9
Diyorbekkdev <diyorbekjuraev77@gmail.com>
2023-06-29 19:29:26 +0500

    some parts updated

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// const axios = require('axios');
+
+// const url = 'https://topkitob.com/api/customers';
+// const data = {
+//   userName: 'diyorbek',
+//   name: 'kxfjgjdfgffdg',
+//   phone1: 's4465465fgring',
+//   phone2: '65651654',
+//   notes: 'string'
+// };
+
+// axios.post(url, data, {
+//   headers: {
+//     'Content-Type': 'application/json'
+//   }
+// })
+// .then(response => {
+//   console.log(response.config.data);
+// })
+// .catch(error => {
+//   console.error(error);
+// });
