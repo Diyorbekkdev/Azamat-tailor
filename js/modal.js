e9a49e9473f76183a228295c938e3fb9b7365f4d
numonovnodirbek <nodirbeknumonov2007@gmail.com>
2023-07-03 02:12:17 -0700

    nodirbek

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,9 @@
 const openModal = document.querySelector(".add-order-btn");
 const modalOverlay = document.querySelector(".modal-overlay");
-const modalCloseBtn = document.querySelector(".close");
+const modalCloseBtn = document.querySelector("#close");
+const closeYangi = document.querySelector("#closeYangi");
+const openYangi = document.querySelector(".new-customer-modal");
+const orderModal = document.querySelector(".add-order-modal");
 
 openModal.addEventListener("click", modalActive);
 
@@ -19,69 +22,14 @@ window.addEventListener("click", function (e) {
 });
 
 
-let choices = [
-  "Diyorbek",
-  "Nodirbek",
-  "Javlon",
-  "Durbek",
-  "What does CSS stands for",
-  "Facebook login",
-  "Login box with HTML & CSS",
-  "Who is a Freelancer",
-  "How to create an Instagram page",
-  "What is a text generator",
-  "Who is Lorem Ipsum",
-  "Backend developer",
-  "Learn to be a backend developer",
-  "Frontend developer",
-  "Web designer",
-  "Javlon",
-];
-let $ = document;
-let list_group = $.querySelector(".customer_list");
-
-function ListItemGenerator() {
-  if (!inputTxt.value) {
-    inputTxt.parentElement.classList.remove("active");
-    list_group.style.display = "none";
-  } else {
-    inputTxt.parentElement.classList.add("active");
-    list_group.style.display = "block";
-
-    let SearchResults = choices.filter(function (choice) {
-      return choice.toLowerCase().startsWith(inputTxt.value.toLowerCase());
-    });
-
-    CreatingList(SearchResults);
-
-    function CreatingList(Words) {
-      let createdList = Words.map(function (word) {
-        return "<li>" + word + "</li>";
-      });
-      let CustomListItem;
-      if (!CreatingList.length) {
-        CustomListItem = "<li>" + inputTxt.value + "</li>";
-      } else {
-        CustomListItem = createdList.join("");
-      }
-      list_group.innerHTML = CustomListItem;
-      CompleteText();
-    }
-  }
-
-  function CompleteText() {
-    all_list_items = list_group.querySelectorAll("li");
-    all_list_items.forEach(function (list) {
-      list.addEventListener("click", function (e) {
-        inputTxt.value = e.target.textContent;
-        list_group.style.display = "none";
-      });
-    });
-  }
+function openYangiModal(){
+  openYangi.classList.add('yangiActive');
+  orderModal.style.transform = "scale(.9)"
 }
 
-inputTxt.addEventListener("keyup", ListItemGenerator);
+function closeYangiModal(){
+  openYangi.classList.remove("yangiActive");
+  orderModal.style.transform = "scale(1)";
+}
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("phone").mask("+998(99)999-99-99");
-});
+// =======AUTOCOMPLETE=======//
